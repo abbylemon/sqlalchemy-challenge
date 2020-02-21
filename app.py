bec2e6226d760e6bfb4b674284da1b67401d4f4a
physics_nerd <abby.lemon22@gmail.com>
2020-02-21 09:11:20 -0600

    got first flask query and api to work for precipitation

--- app.py
+++ app.py
@@ -1,18 +1,82 @@
-from flask import Flask
+from flask import Flask, jsonify
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+import numpy as np
+import datetime as dt
+
+
+####################################################################
+# Database Setup
+####################################################################
+
+engine = create_engine('sqlite:///Resources/hawaii.sqlite')
+
+# reflect database into a model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save reference to the table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+####################################################################
+# Flask Setup
+####################################################################
 
 app = Flask(__name__)
 
+####################################################################
+# Flask Routes
+####################################################################
+
 @app.route("/")
 def home():
-    print("Welcome to my home page....")
-    print("For more content go to:")
-    print("/api/v1.0/precipitation")
-    print("/api/v1.0/stations")
-    print("/api/v1.0/tobs")
-    print("/api/v1.0/<start>")
-    print("/api/v1.0/<start>/<end>")
+    return(
+        f"Welcome to my home page!<br/>"
+        f"For more content go to:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start><br/>"
+        f"/api/v1.0/<start>/<end><br/>"
+    )
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    # Create our session from Python to the DB
+    session = Session(engine)
     
+    # Query
+    lastestmeasurement = session.query(Measurement.date).\
+    order_by(Measurement.date.desc()).first()
+    
+    twelveMonthsAgo = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+
+    lastyearofdata = session.query(Measurement.date, Measurement.prcp).\
+    filter(Measurement.date > twelveMonthsAgo).\
+    order_by(Measurement.date.desc()).all()
+    
+    session.close()
+    
+    # Create a list of dictionaries from the data queried above
+    all_precipitation = []
+    for date, prcp in lastyearofdata:
+        precipitation_dict = {}
+        precipitation_dict['date'] = date
+        precipitation_dict['prcp'] = prcp
+        all_precipitation.append(precipitation_dict)
+        
+    return jsonify(all_precipitation)
     
     
 if __name__ == '__main__':
     app.run(debug=True)
+    
+    
+    
+    
